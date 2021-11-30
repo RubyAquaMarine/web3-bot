978970d4e94cd1638629c88e1370cfe29ce6da08
Aqua Marine <aquamarine@ruby.exchange>
2021-11-30 18:44:57 +0700

    RubyExchange Working

---
+++ SKALE.md
@@ -0,0 +1,11 @@
+# Notes
+
+skaled is designed to handle 1 transaction from 1 account per block, so you have to wait till the previous transaction will be mined. So, answering your question, it's not something SDK-specific, you'll get the same results on the real SKALE Chain. 
+
+FYI, we're working on a 'high-speed mode' that will allow users to send multiple transactions in one block. This feature is scheduled for the next major release
+
+# Block Explorer
+- [URL](http://melodic-murzim.explorer.dappnet.skalenodes.com/accounts) 
+
+# RPC
+- https://dappnet-api.skalenodes.com/v1/melodic-murzim
---
+++ bot_ruby.js
@@ -0,0 +1,225 @@
+const { getAddress } = require('@ethersproject/address');
+const ethers = require('ethers');
+const routerABI = require('./abi/ruby_router.json');
+const erc20ABI = require('./abi/erc20.json');
+const credentials = require('./config/config.json');
+
+const provider = new ethers.providers.JsonRpcProvider(credentials.rpc.schainRuby);
+
+const privateKey = credentials.account.rubyKey;
+
+const publicAddress = credentials.account.rubyAddress;
+
+const wallet = new ethers.Wallet(privateKey);
+// creates the Signer abstract class => into the Signer client
+// using one of the following  Wallet, VoidSigner, JsonRpcSigner
+const account = wallet.connect(provider);
+
+const timerSpeed = credentials.tools.m1;
+
+const routerContract = new ethers.Contract(credentials.rubyexchange.router, routerABI, account);
+
+const fromToken = credentials.swap.fromAddress;
+
+const toToken = credentials.swap.toAddress;
+
+const fromContract = new ethers.Contract(credentials.swap.fromAddress, erc20ABI, account);
+
+/*
+working
+    - Check the allowance amount given to the router
+    - Increase the allowance if necessary
+    - Function should be checked before each swap to ensure the transaction can go through (refactor later)
+*/
+
+async function doApproval() {
+
+    const weiAmount = ethers.utils.parseUnits(credentials.swap.amount, 'ether');
+
+    let allowanceAmount = await fromContract.allowance(publicAddress, credentials.rubyexchange.router);
+
+    console.log("Router Contract Allowance: " + allowanceAmount.toString(), allowanceAmount);
+
+    if (allowanceAmount.toString() == "0" || weiAmount.toString() >= allowanceAmount.toString()) {
+
+        console.log("Router Contract Needs Increased Allowance: ");
+
+        const increase = weiAmount.mul(10);
+
+        const parse = await fromContract.approve(credentials.rubyexchange.router, increase);
+
+        const receipt = await parse.wait();
+
+        console.log("Router Contract Result: ", receipt);
+    }
+}
+
+
+
+async function doStuff() {
+
+    //Provider 
+    const blockNumber = await provider.getBlockNumber();
+
+    const balance = await provider.getBalance(publicAddress);
+
+    const gas_try = await provider.getGasPrice();
+
+    const blockData = await provider.getBlock(blockNumber);
+
+    const expiryDate = ethers.BigNumber.from(blockData.timestamp + 23600);
+
+    console.log(" BlockNumber: " + blockNumber + " Balance: " + balance + " GasPrice: " + gas_try.toString() + "\nExpires: " + expiryDate + " | Time: " + blockData.timestamp);
+
+    //Signer 
+    const address = await account.getAddress();
+
+    const nonce = await account.getTransactionCount("latest");
+
+    console.log(" My Wallet Address: " + address + " TransactionCount: " + nonce);
+
+    let decimalDigit = await fromContract.decimals();
+
+    const originalAmount = credentials.swap.amount;
+
+    // let try_new = ethers.utils.formatUnits(originalAmount, 6);// 1 turns into 0.000001
+
+    const weiAmount = ethers.utils.parseUnits(originalAmount, decimalDigit);
+    // const weiAmount = ethers.utils.parseUnits(originalAmount, 'ether');
+
+    const price_try = await routerContract.getAmountsOut(weiAmount, [fromToken, toToken]);
+
+    const amountOut = price_try[1].sub(price_try[1].div(10));// 10% slippage
+
+    let try_string = gas_try.toString();
+
+    console.log("AmountsINN: ", weiAmount.toString());
+    console.log("AmountsOUT: ", amountOut.toString());
+
+    var swap_tx = '';
+
+
+    if (credentials.swap.type == 'A') {
+        swap_tx = await routerContract.swapExactETHForTokens(
+            amountOut,// amountOutMin
+            [fromToken, toToken],
+            publicAddress,
+            expiryDate,
+
+            {
+                "gasPrice": try_string,
+                "gasLimit": "280000",
+                "nonce": nonce,
+                "value": weiAmount
+            }
+
+        ).then(result => {
+            // result is another promise =. deal with it 
+            let out = result.wait().then(ok => {
+                console.log("Result: ", ok);
+            }).catch(err => {
+                console.log("Result Error: ", err);
+            });
+        }).catch(err => {
+            console.log("Processing Error: ", err);
+        });
+        //   let receipt = await swap_tx.wait(); // wait for 1 block
+        //   console.log("SwapReceipt: ", receipt); // sanity check
+    }
+
+    if (credentials.swap.type == 'B') {
+        swap_tx = await routerContract.swapExactTokensForETH(
+            weiAmount,// amountIn
+            amountOut,// amountOutMin
+            [fromToken, toToken],
+            publicAddress,
+            expiryDate,
+
+            {
+                "gasPrice": try_string,
+                "gasLimit": "280000",
+                "nonce": nonce,
+            }
+
+        ).then(result => {
+            // result is another promise =. deal with it 
+            let out = result.wait().then(ok => {
+                console.log("Result: ", ok);
+            }).catch(err => {
+                console.log("Result Error: ", err);
+            });
+        }).catch(err => {
+            console.log("Processing Error: ", err);
+        });
+        //   let receipt = await swap_tx.wait(); // wait for 1 block
+        //   console.log("SwapReceipt: ", receipt); // sanity check
+    }
+
+    if (credentials.swap.type == 'C') {
+        swap_tx = await routerContract.swapExactTokensForTokens(
+            weiAmount,// amountIn
+            amountOut,// amountOuMin
+            [fromToken, toToken],
+            publicAddress,
+            expiryDate,
+
+            {
+                "gasPrice": try_string,
+                "gasLimit": "280000",
+                "nonce": nonce
+            }
+
+        ).then(result => {
+            // result is another promise =. deal with it 
+            let out = result.wait().then(ok => {
+                console.log("Result: ", ok);
+            }).catch(err => {
+                console.log("Result Error: ", err);
+            });
+        }).catch(err => {
+            console.log("Processing Error: ", err);
+        });
+        //   let receipt = await swap_tx.wait(); // wait for 1 block
+        //   console.log("SwapReceipt: ", receipt); // sanity check
+    }
+
+    if (credentials.swap.type == 'D') {
+        swap_tx = await routerContract.swapTokensForExactTokens(
+            amountOut,// amountOut
+            weiAmount,// amountInMax
+            [fromToken, toToken],
+            publicAddress,
+            expiryDate,
+
+            {
+                "gasPrice": try_string,
+                "gasLimit": "280000",
+                "nonce": nonce
+            }
+
+        ).then(result => {
+            // result is another promise =. deal with it 
+            let out = result.wait().then(ok => {
+                console.log("Result: ", ok);
+            }).catch(err => {
+                console.log("Result Error: ", err);
+            });
+        }).catch(err => {
+            console.log("Processing Error: ", err);
+        });
+        //  let receipt = await swap_tx.wait(); // wait for 1 block
+        //  console.log("SwapReceipt: ", receipt); // sanity check
+    }
+
+
+};
+
+
+function run() {
+    console.log("Bot is running... after timerSpeed expires, the bot will perform an action");
+    doApproval();
+    setInterval(doStuff, timerSpeed);
+};
+
+run();
+
